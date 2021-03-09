e1fc2fc53783c3d7cfcca814caf57e634e00e811
Konstantin8105 <Konstantin8105@gmail.com>
2021-03-09 10:26:09 +0300

    not compiled

--- build.sh
+++ build.sh
@@ -2,8 +2,23 @@
 
 echo "Build and test"
 
-mkdir testdata
 
+echo "Create templorary folder"
+mkdir -p ./testdata/
+
+echo "Unzip code archive"
+unzip ./NLS/Codes/NLS_Code.zip -d ./testdata/ > /dev/null
+mv "./testdata/NLS Code" "./testdata/NLSCode"
+
+cd ./testdata/NLSCode/src
+make
+
+cd ../../..
+
+cd ./testdata/NLSCode/tests/
+make
+
+cd ../../..
 
 # Remove templorary data
 # rm -rf testdata
